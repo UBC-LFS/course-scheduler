f2626a9d3477075647f6d3df3149447443a1a7fa
Justin Lee <justinlee0022@gmail.com>
2017-08-30 16:07:59 -0700

    Iterating over every department code and running fetch on each one

--- src/coursesAPI/constants.js
+++ src/coursesAPI/constants.js
@@ -1,10 +1,11 @@
 export const baseURL = `https://courses.students.ubc.ca/cs/servlets/SRVCourseSchedule?`
-export const departments = []
+export const departments = ['AANB', 'APBI', 'FNH', 'FOOD', 'FRE', 'GRS', 'HUNU', 'LFS', 'SOIL']
 export const and = '&'
 export const year = 'sessyr=2017'
 export const term = 'sesscd=W'
 export const req4 = 'req=4'
 export const req3 = 'req=3'
+export const req2 = 'req=2'
 export const dept = (deptCode) => `dept=${deptCode}`
 export const course = (courseCode) => `course=${courseCode}`
 export const output = 'output=3'
--- src/coursesAPI/coursesAPI.js
+++ src/coursesAPI/coursesAPI.js
@@ -1,15 +1,24 @@
 import X2JS from 'x2js'
-import { baseURL, and, year, term, req4, req3, dept, course, output } from './constants.js'
+import { baseURL, and, year, term, req4, req3, req2, dept, course, output, departments } from './constants.js'
 
 const x2js = new X2JS();
 
 const getCoursesForCode = (code) => {
-    //fetch(c.baseURL + c.year + c.term + c.req4 + c.dept('APBI') + c.course(200) + c.output)
-    fetch(baseURL + and + year + and + term + and + req4 + and + dept('APBI') + and + course(200) + and + output)
+    //fetch(baseURL + and + year + and + term + and + req4 + and + dept('APBI') + and + course(200) + and + output)
+    fetch(baseURL + and + year + and + term + and + req2 + and + dept(code) + and + output)
         .then(response => response.text())
         .then(text => x2js.xml2js(text))
         .then(json => console.log(JSON.stringify(json, null, 2)))
 }
 
-export default getCoursesForCode
+const main = () => {
+    departments.map(code => getCoursesForCode(code))
+        .then(result => console.log(result))
+}
+
+
+
+export default main
+
+
 //'https://courses.students.ubc.ca/cs/servlets/SRVCourseSchedule?&sessyr=2017&sesscd=W&req=4&dept=APBI&course=200&output=3'
