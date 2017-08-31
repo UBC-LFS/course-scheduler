4057b2d411e03dd06a45a32672f5ef1094b48639
Justin Lee <justinlee0022@gmail.com>
2017-08-31 14:49:47 -0700

    parseOutSections works as expected!

--- src/coursesAPI/coursesAPI.js
+++ src/coursesAPI/coursesAPI.js
@@ -1,11 +1,12 @@
 import X2JS from 'x2js'
 import { baseURL, and, year, term, req4, req3, req2, dept, course, output, departments, scrapeURL } from './constants.js'
-import APBI200 from '../sampleJSON/APBI200'
 import request from 'request'
 import cheerio from 'cheerio'
 
-const x2js = new X2JS();
+import APBI200 from '../sampleJSON/APBI200'
+import anotherSection from '../sampleJSON/anotherSection'
 
+const x2js = new X2JS();
 
 // scrape website for enrolment data
 const getEnrolmentInfo = (code, number, section, callback) => {
@@ -24,31 +25,12 @@ const getEnrolmentInfo = (code, number, section, callback) => {
         const generalSeatsRemaining = getNumberFromTD('General Seats Remaining:')
         const restrictedSeatsRemaining = getNumberFromTD('Restricted Seats Remaining*:')
 
-        callback({
-            totalSeatsRemaining,
-            currentlyRegistered,
-            generalSeatsRemaining,
-            restrictedSeatsRemaining
-        })
+        callback({ totalSeatsRemaining, currentlyRegistered, generalSeatsRemaining, restrictedSeatsRemaining })
     })
 }
 
-// const changeSections = (sectionObj) => {
-//     sectionObj.sections.section.map(oneSection => {
-//         oneSection.teachingunits.teachingunit.meetings.meeting.map(y => console.log(y))
-//     })
-// } 
+const parseOutSections = (sectionsBlob) => sectionsBlob.sections.section.map(section => section._key)
 
-// const buildJSONOutput = (code, number, sections) => {
-//     const result = {
-//         number: [
-//             sections
-//         ]
-//     }
-//     //console.log(JSON.stringify(sections, null, 2))
-// }
-
-// gets all course numbers given a department code
 const getCoursesForCode = (code) => {
     //fetch(baseURL + and + year + and + term + and + req4 + and + dept('APBI') + and + course(200) + and + output)
     return fetch(baseURL + and + year + and + term + and + req2 + and + dept(code) + and + output)
@@ -58,12 +40,11 @@ const getCoursesForCode = (code) => {
 }
 
 const getSectionsForCourse = ({ code, courseNumbers }) => {
-
     courseNumbers.map(number => {
         fetch(baseURL + and + year + and + term + and + req4 + and + dept(code) + and + course(Number(number)) + and + output)
             .then(response => response.text())
             .then(text => x2js.xml2js(text))
-            // .then(sections => buildJSONOutput(code, number, sections))
+            //.then(result => console.log(JSON.stringify(result, null, 2)))
     })
 } 
 
@@ -75,15 +56,18 @@ const main = () => {
                 code,
                 courseNumbers: courseNumbers
             }
-            //getSectionsForCourse(codeAndNumbers)
+            getSectionsForCourse(codeAndNumbers)
         })
     )
+
+    console.log(parseOutSections(anotherSection))
+
     //changeSections(APBI200)
-    getEnrolmentInfo('APBI', 200, '001', (data) => console.log('APBI200', data))
-    getEnrolmentInfo('LFS', 500, '001', (data) => console.log('LFS500', data))
-    getEnrolmentInfo('APBI', 398, '001', (data) => console.log('APBI398', data))
-    getEnrolmentInfo('APBI', 361, '199', (data) => console.log('APBI361', data))
-    getEnrolmentInfo('APBI', 360, '001', (data) => console.log('APBI360', data))
+    // getEnrolmentInfo('APBI', 200, '001', (data) => console.log('APBI200', data))
+    // getEnrolmentInfo('LFS', 500, '001', (data) => console.log('LFS500', data))
+    // getEnrolmentInfo('APBI', 398, '001', (data) => console.log('APBI398', data))
+    // getEnrolmentInfo('APBI', 361, '199', (data) => console.log('APBI361', data))
+    // getEnrolmentInfo('APBI', 360, '001', (data) => console.log('APBI360', data))
 }
 
 export default main
