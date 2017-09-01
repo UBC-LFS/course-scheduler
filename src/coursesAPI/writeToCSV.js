c128b1d6e8867a25a98363812f7f24a6e172e382
Justin Lee <justinlee0022@gmail.com>
2017-09-01 13:01:07 -0700

    got rid of test jsons

--- src/coursesAPI/writeToCSV.js
+++ src/coursesAPI/writeToCSV.js
@@ -1,69 +1,6 @@
 import fs from 'fs'
 import json2csv from 'json2csv'
 
-
-const testJSON = {
-    dept: 'FRE',
-    course: '518',
-    sectionNumber: '001',
-    meeting:
-    {
-        _term: '2',
-        _day: 'Wed',
-        _starttime: '10:30',
-        _endtime: '12:00',
-        _buildingcd: 'MCML',
-        _building: 'MacMillan',
-        _roomno: '258'
-    },
-    instructors: { 
-        instructor: { 
-            _name: 'WISEMAN, KELLEEN', 
-            _ubcid: '920661' 
-        } 
-    },
-    activity: 'Lecture',
-    credits: '1.5',
-    enrolmentInfo: {
-        totalSeatsRemaining: '25',
-        currentlyRegistered: '0',
-        generalSeatsRemaining: '0',
-        restrictedSeatsRemaining: '25'
-    }
-}
-const testJSONWithTwoInstructors = {
-    dept: 'FRE',
-    course: '518',
-    sectionNumber: '001',
-    meeting:
-    {
-        _term: '2',
-        _day: 'Wed',
-        _starttime: '10:30',
-        _endtime: '12:00',
-        _buildingcd: 'MCML',
-        _building: 'MacMillan',
-        _roomno: '258'
-    },
-    instructors: { 
-        instructor: [{ 
-            _name: 'WISEMAN, KELLEEN', 
-            _ubcid: '920661' 
-        },{ 
-            _name: 'LEE, JUSTIN', 
-            _ubcid: '920661' 
-        } ] 
-    },
-    activity: 'Lecture',
-    credits: '1.5',
-    enrolmentInfo: {
-        totalSeatsRemaining: '25',
-        currentlyRegistered: '0',
-        generalSeatsRemaining: '0',
-        restrictedSeatsRemaining: '25'
-    }
-}
-
 const flattenJSON = ({ dept, course, sectionNumber, meeting, instructors, activity, credits, enrolmentInfo }) => {
     let instructorName
     if (typeof instructors === 'undefined') {
