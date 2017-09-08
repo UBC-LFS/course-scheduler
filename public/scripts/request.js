fcd6fe4f3e55d5877153c789816354d7b5c53b49
Justin Lee <justin.lee@ubc.ca>
2017-09-07 19:29:46 -0700

    added selectivity as dependency

--- public/scripts/request.js
+++ public/scripts/request.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     createDeptCheckboxes()
-    
+
 });
 
 const createDeptCheckboxes = () => {
     fetch('http://localhost:8080/deptCodes')
         .then(response => response.json())
-        .then(json => json.depts.dept.map(x => {
-            const code = x._key
-            const checkboxes = document.getElementById('checkboxes')
-            checkboxes.innerHTML += '<label>' + '<input id="' + code + '" type="checkbox">' + code + '</label>'
+        .then(json => json.depts.dept.map(x => x._key))
+        .then(arrayOfCodes => $('#checkboxes').selectivity({
+            items: arrayOfCodes,
+            multiple: true,
+            placeholder: 'Search for your department codes here'
         }))
 }
+
