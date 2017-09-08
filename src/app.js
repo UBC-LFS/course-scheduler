bd7cb0bac658fc336f4b880f43a733e0a05789e8
Justin Lee <justinlee0022@gmail.com>
2017-09-08 11:12:32 -0700

    trying to fix routing bug

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
-app.use('/', routes);
+app.use('/course-scheduler/', routes);
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
